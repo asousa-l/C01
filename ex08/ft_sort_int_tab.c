3fffe92ff74f8b758bcc18646a39cd87336597fe
Andre Sousa lajas <asousa-l@c1r16s1.42lisboa.com>
2022-01-18 15:26:41 +0000

    a

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 08:33:22 by asousa-l          #+#    #+#             */
+/*   Updated: 2022/01/18 08:56:32 by asousa-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		r;
+	int		ind;
+	bool	swapped;
+
+	r = 0;
+	while (true)
+	{
+		swapped = false;
+		ind = 0;
+		while (index < size)
+		{
+			if (tab[ind] > tab[ind + 1])
+			{
+				ft_swap(&tab[ind], &tab[ind + 1]);
+				swapped = true;
+			}
+			index++;
+		}
+		if (!swapped)
+		{
+			break ;
+		}
+		r++;
+	}
+}
